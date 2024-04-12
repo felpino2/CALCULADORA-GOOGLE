cf1a1123b5600b346750befc40b499798927a00d
Georges-Andraus <Georges.andraus@sempreceub.com>
2024-04-12 04:50:36 -0400

    first commit for calculator

---
+++ calculatorController.go
@@ -0,0 +1,17 @@
+package main
+
+func sum(float32 a, float32 b) float32 {
+	return a+b
+}
+
+func subtraction(float32 a, float32 b) float32 {
+	return a-b
+}
+
+func multiply(float32 a, float32 b) float32 {
+	return a*b
+}
+
+func division (float32 a, float32 b) float32 {
+	return ab
+}
