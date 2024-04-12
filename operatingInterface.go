42cbca725240cd1819cefd9f2325f1f400d7f08c
FELIPE AUGUSTO <felipeaugusto2002@sempreceub.com>
2024-04-12 07:20:41 -0400

    var map

--- operatingInterface.go
+++ operatingInterface.go
@@ -4,3 +4,12 @@ type Operating interface {
 	
 	Operate(float32, float32) Result
 }
+
+var CalculatorMap = map[string]Operating{
+	"sum": Sum{},
+	"subtraction": Subtraction{},
+	"multiply": Multiply{},
+	"division": Division{},
+	"pow": Pow{}
+	"root": Root{}
+}
--- sum.go
+++ sum.go
@@ -4,6 +4,13 @@ type Sum struct {
 	arithmicOperation
 }
 
-func (Sum) Operate() Result{
+func (s Sum) Constructor(numero1 float32, numero2 float32) {
+
+	s.Number1 = numero1
+	s.Number2 = numero2
+	return s
+}
+
+func (Sum) Operate(float32, float32) Result{
 	return arithmicOperation.Number1+arithmicOperation.Number2
 }
